c14c89dc66544fed7755441287791a3bb7956661
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2025-08-26 16:33:50 +0530

    Update comprehension_tests.py

--- comprehension_tests.py
+++ comprehension_tests.py
@@ -3,3 +3,7 @@ def test_q1(func):
 
 def test_q2(func):
   return (func([5, 7, -9, 8, 0]) == [25, 81, 0])
+
+def test_q3(func) :
+  return func(sentence) == ['quick', 'brown', 'jumps', 'over', 'lazy']
+
