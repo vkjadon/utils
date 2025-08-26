7446d38a39e441847188681ca380a3569b9647fb
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2025-08-26 14:28:21 +0530

    Update comprehension_tests.py

--- comprehension_tests.py
+++ comprehension_tests.py
@@ -1,2 +1,5 @@
 def test_q1(func):
   return (func("Vijay") == ["v", "j", "y"] and func("Mechanical") == ["m", "c", "a", "i", "a"])
+
+def test_q2(func):
+  return (func([5, 7, -9, 8, 0]) == [25, 81, 0])
